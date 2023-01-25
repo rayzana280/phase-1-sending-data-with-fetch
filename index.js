06539304bd776b3c725858b24098244d6ef75bf5
Ray Zana <rayzana280@gmail.com>
2023-01-24 20:23:23 -0500

    commit tree 92bad632bda7ce9ce5396d85b57394031dfc6550

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(username, email){
+    const formatData = {
+        name: username,
+        email: email
+    };
+    const setObject = {
+        method: 'POST',
+        body: JSON.stringify(formatData),
+        headers:{
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+    };
+    return fetch('http://localhost:3000/users', setObject)
+        .then(response => response.json())
+        .then(data => {
+            const newElement = document.createElement('h1')
+            newElement.innerHTML = String(data.id);
+            document.body.appendChild(newElement)
+        })
+        .catch(error => {
+            const newElement = document.createElement('h1')
+            newElement.innerHTML = error.message
+            document.body.appendChild(newElement)
+        })
+}
+
+submitData();
